7d62e23c3d0075a42baac147de5e87099f9017af
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-11-01 13:22:31 +0100

    [ADD] cache import;

--- odoo_eshop/eshop_app/application.py
+++ odoo_eshop/eshop_app/application.py
@@ -7,15 +7,22 @@ from datetime import timedelta
 # Extra Lib
 from flask import Flask
 from flask.ext.babel import Babel
+from flask.ext.cache import Cache
 
 # Custom Tools
 from tools.config import conf
 
 
+# Create Aplication
 app = Flask(__name__)
 app.secret_key = conf.get('flask', 'secret_key')
 app.debug = conf.get('flask', 'debug') == 'True'
 app.config['BABEL_DEFAULT_LOCALE'] = conf.get('localization', 'locale')
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(
     minutes=int(conf.get('auth', 'session_minute')))
+
+# Manage translation for the new application
 babel = Babel(app)
+
+# Manage Cache for the new application
+cache = Cache(app,config={'CACHE_TYPE': conf.get('cache', 'method')})
