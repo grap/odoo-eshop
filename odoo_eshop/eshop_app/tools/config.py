2e988af2fa5267d09ea992e37c6ce739b0bb44e0
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-11-12 01:36:51 +0100

    [FIX] incorrect path after refactoring;

--- odoo_eshop/eshop_app/tools/config.py
+++ odoo_eshop/eshop_app/tools/config.py
@@ -14,7 +14,7 @@ def read_config():
         config_file = '/etc/odoo_eshop/config.ini'
     if not isfile(config_file):
         config_file = dirname(realpath(__file__))\
-            + '/../config/config.ini'
+            + '/../../../config/config.ini'
     assert isfile(config_file), (
         'Could not find config file (looking at %s)' % (
             config_file or "~/.odoo_eshop.ini', /etc/odoo_eshop/config.ini" +
