a1ef2d4bddc4058f0be5a1cc3ea030b18eb32949
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-07-19 12:14:43 +0200

    [IMP] make database login working on test servers, with changing database names

--- odoo_eshop/eshop_app/tools/erp.py
+++ odoo_eshop/eshop_app/tools/erp.py
@@ -1,5 +1,3 @@
-import base64
-
 import odoorpc
 
 from .config import conf
@@ -7,6 +5,24 @@ from .config import conf
 
 def init_odoo(host, port, login, password, database):
     odoo = odoorpc.ODOO(host, 'jsonrpc', port)
+    db_list = odoo.json('/web/database/list', {})["result"]
+    db_list.sort(reverse=True)
+
+    if database not in db_list:
+        # If the exact database is not found,
+        # me make a fuzzy search. It makes the eshop working
+        # on test servers, with database like
+        # 'company_name_production__xx_xx_xx'
+
+        for db in db_list:
+            if db.startswith(database):
+                database = db
+                break
+
+    if database not in db_list:
+        raise Exception(
+            ("Database %s not found. (fuzzy search failed)" % database)
+        )
     uid = odoo.login(database, login, password)
     user = odoo.env['res.users'].browse(uid)
     tz = user.tz
