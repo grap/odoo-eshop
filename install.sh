0b0785f8ad8325c3013c47d6aa7132cfe7691e7c
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-25 12:36:18 +0200

    [FIX] add redis-server in the installation

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ virtualenv env --python=python3
 ./env/bin/pip install -r ./requirements.txt
 mkdir ./odoo_eshop/eshop_app/static/odoo_data/
 cp ./config/config.ini.sample ./config/config.ini
+
+sudo apt-get install redis-server -y
