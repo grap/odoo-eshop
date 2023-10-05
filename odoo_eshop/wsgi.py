4ddcd52ee486ac9cab6a634e95d726696548eecc
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-10-05 10:20:52 +0200

    [ADD] et ouais gros

---
+++ odoo_eshop/wsgi.py
@@ -0,0 +1,16 @@
+from odoo_eshop.eshop_app.application import app as application
+
+
+
+# from .eshop_app.tools.config import conf
+
+# app.run(
+#     host=conf.get("flask", "host"),
+#     port=conf.getint("flask", "port"),
+#     debug=conf.get("flask", "debug"),
+# )
+
+## Sur le serveur
+
+# sudo apt-get install uwsgi
+# sudo apt-get install uwsgi-plugin-python3
