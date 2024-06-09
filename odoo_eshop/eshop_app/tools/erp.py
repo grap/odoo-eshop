f52059a99c9ec8359bc225f0b1e4242db3a75514
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-06-10 01:35:41 +0200

    [FIX] do not raise an error if database/list is disabled on odoo server

--- odoo_eshop/eshop_app/tools/erp.py
+++ odoo_eshop/eshop_app/tools/erp.py
@@ -5,22 +5,27 @@ from .config import conf
 
 def init_odoo(host, port, login, password, database):
     odoo = odoorpc.ODOO(host, "jsonrpc", port)
-    db_list = odoo.json("/web/database/list", {})["result"]
-    db_list.sort(reverse=True)
+    try:
+        # This will fail if /database/list is disabled on server
+        db_list = odoo.json("/web/database/list", {})["result"]
+        db_list.sort(reverse=True)
 
-    if database not in db_list:
-        # If the exact database is not found,
-        # me make a fuzzy search. It makes the eshop working
-        # on test servers, with database like
-        # 'company_name_production__xx_xx_xx'
+        if database not in db_list:
+            # If the exact database is not found,
+            # me make a fuzzy search. It makes the eshop working
+            # on test servers, with database like
+            # 'company_name_production__xx_xx_xx'
 
-        for db in db_list:
-            if db.startswith(database):
-                database = db
-                break
+            for db in db_list:
+                if db.startswith(database):
+                    database = db
+                    break
 
-    if database not in db_list:
-        raise Exception(("Database %s not found. (fuzzy search failed)" % database))
+        if database not in db_list:
+            raise Exception(("Database %s not found. (fuzzy search failed)" % database))
+    except odoorpc.error.RPCError:
+        pass
+
     uid = odoo.login(database, login, password)
     user = odoo.env["res.users"].browse(uid)
     tz = user.tz
